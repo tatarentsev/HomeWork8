8d15d324e34257c20dff8ac51a9bd2db3196d862
ilyaTATARENTSEV <117466522+tatarentsev@users.noreply.github.com>
2023-02-12 18:51:14 +0300

    Task54

---
+++ Example54.cs
@@ -0,0 +1,50 @@
+﻿int[,] arr = new int[4, 4];
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+CreateArray(arr);
+
+void SortedArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j], 5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+SortedArray(arr);
+PrintArray(arr);
+Console.ReadLine();
